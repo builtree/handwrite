e3ff34811d50855da0249dc57d28596704546362
sakshamarora1 <sakshamarora1001@gmail.com>
2021-02-19 14:43:14 +0530

    setup: Update 0.1.1 to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="handwrite",
-    version="0.1.1",
+    version="0.2.0",
     author="Yash Lamba, Saksham Arora, Aryan Gupta",
     author_email="yashlamba2000@gmail.com, sakshamarora1001@gmail.com, aryangupta973@gmail.com",
     description="Convert text to custom handwriting",
