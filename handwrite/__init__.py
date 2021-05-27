25027a39f4cd1fdd61b8e1be214c762d41b1ed47
sakshamarora1 <sakshamarora1001@gmail.com>
2021-05-28 03:28:35 +0530

    cli: Refactored code to extend functionality to use tempdir through Python API too

--- handwrite/__init__.py
+++ handwrite/__init__.py
@@ -1,3 +1,4 @@
 from handwrite.sheettopng import SheetToPNG
 from handwrite.pngtosvg import PngToSvg
 from handwrite.svgtottf import SVGtoTTF
+from handwrite.cli import converters
--- handwrite/cli.py
+++ handwrite/cli.py
@@ -8,10 +8,56 @@ from handwrite import PngToSvg
 from handwrite import SVGtoTTF
 
 
-def converters(sheet, characters_dir, output_directory, config):
+def run(sheet, output_directory, characters_dir, config):
     SheetToPNG().convert(sheet, characters_dir, config)
     PngToSvg().convert(directory=characters_dir)
     SVGtoTTF().convert(characters_dir, output_directory, config)
+
+
+def converters(sheet, output_directory, directory=None, config=None):
+    if not directory:
+        directory = tempfile.mkdtemp()
+        isTempdir = True
+    else:
+        isTempdir = False
+
+    default_config = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "default.json"
+    )
+    if config is None:
+        config = default_config
+
+    configs_dir = None
+    if os.path.isdir(config):
+        configs_dir = config
+
+    if os.path.isdir(sheet):
+        configs_dir = configs_dir or directory + os.sep + "configs"
+        os.makedirs(configs_dir, exist_ok=True)
+
+        for sheet_name in sorted(os.listdir(sheet)):
+            config_file = (
+                configs_dir + os.sep + os.path.splitext(sheet_name)[0] + ".json"
+            )
+            if not os.path.exists(config_file):
+                if os.path.isdir(config):
+                    shutil.copyfile(default_config, config_file)
+                else:
+                    shutil.copyfile(config, config_file)
+
+            characters_dir = directory + os.sep + os.path.splitext(sheet_name)[0]
+            os.makedirs(characters_dir, exist_ok=True)
+            run(
+                sheet + os.sep + sheet_name,
+                output_directory,
+                characters_dir,
+                config_file,
+            )
+    else:
+        run(sheet, output_directory, directory, config)
+
+    if isTempdir:
+        shutil.rmtree(directory)
 
 
 def main():
@@ -28,47 +74,6 @@ def main():
     parser.add_argument(
         "--config", help="Use custom configuration file/directory", default=None
     )
+
     args = parser.parse_args()
-
-    directory = args.directory
-    if not directory:
-        directory = tempfile.mkdtemp()
-
-    default_config = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), "default.json"
-    )
-    if args.config is None:
-        args.config = default_config
-
-    configs_dir = None
-    if os.path.isdir(args.config):
-        configs_dir = args.config
-
-    if os.path.isdir(args.input_path):
-        configs_dir = configs_dir or directory + os.sep + "configs"
-        os.makedirs(configs_dir, exist_ok=True)
-
-        for sheet_name in sorted(os.listdir(args.input_path)):
-            config_file = (
-                configs_dir + os.sep + os.path.splitext(sheet_name)[0] + ".json"
-            )
-            if not os.path.exists(config_file):
-                if os.path.isdir(args.config):
-                    shutil.copyfile(default_config, config_file)
-                else:
-                    shutil.copyfile(args.config, config_file)
-
-            characters_dir = directory + os.sep + os.path.splitext(sheet_name)[0]
-            os.makedirs(characters_dir, exist_ok=True)
-
-            converters(
-                args.input_path + os.sep + sheet_name,
-                characters_dir,
-                args.output_directory,
-                config_file,
-            )
-    else:
-        converters(args.input_path, directory, args.output_directory, args.config)
-
-    if not args.directory:
-        shutil.rmtree(directory)
+    converters(args.input_path, args.output_directory, args.directory, args.config)
